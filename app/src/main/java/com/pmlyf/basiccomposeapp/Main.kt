750e6fbdd5c9f759cb418895189b68700a7253cc
PMLyf <david.irvinge@gmail.com>
2024-05-31 19:10:53 -0400

    Created a main function

---
+++ app/src/main/java/com/pmlyf/basiccomposeapp/Main.kt
@@ -0,0 +1,10 @@
+package com.pmlyf.basiccomposeapp
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+
+class Main: ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+    }
+}
